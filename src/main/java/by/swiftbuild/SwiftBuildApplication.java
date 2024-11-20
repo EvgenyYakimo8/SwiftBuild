00763f74adf19c34a731a6c6bae563369ab94da9
EvgenyYakimo8 <colos0117@gmail.com>
2024-11-20 13:21:49 +0300

    Initial Commit (Reapply)

--- src/main/java/by/swiftbuild/SwiftBuildApplication.java
+++ src/main/java/by/swiftbuild/SwiftBuildApplication.java
@@ -8,6 +8,8 @@ public class SwiftBuildApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SwiftBuildApplication.class, args);
+        //qweqweqwe
     }
+
 
 }
