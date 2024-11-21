352bfd6baaee7d8ba223a53960dc19e9ce67e044
EvgenyYakimo8 <colos0117@gmail.com>
2024-11-21 11:09:12 +0300

    Add Registration and Login

--- src/main/java/by/swiftbuild/SwiftBuildApplication.java
+++ src/main/java/by/swiftbuild/SwiftBuildApplication.java
@@ -8,7 +8,6 @@ public class SwiftBuildApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SwiftBuildApplication.class, args);
-        //qweqweqwe
     }
 
 
