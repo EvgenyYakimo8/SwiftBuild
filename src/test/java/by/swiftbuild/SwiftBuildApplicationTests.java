7d937fc2f2ebcde69f9d36e87573978cde8fd21f
EvgenyYakimo8 <164914498+EvgenyYakimo8@users.noreply.github.com>
2025-01-23 08:53:01 +0300

    Update SwiftBuildApplicationTests.java

--- src/test/java/by/swiftbuild/SwiftBuildApplicationTests.java
+++ src/test/java/by/swiftbuild/SwiftBuildApplicationTests.java
@@ -15,46 +15,5 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 class SwiftBuildApplicationTests {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    /*private String obtainAccessToken() throws Exception {
-        mockMvc.perform(post("/user/reg")
-                        .content("{\"username\":\"testuser\", \"password\":\"testpassword\"}")
-                        .contentType("application/json"))
-                .andExpect(status().isOk());
-
-        MvcResult result = mockMvc.perform(post("/user/login")
-                        .content("{\"username\":\"testuser\", \"password\":\"testpassword\"}")
-                        .contentType("application/json"))
-                .andExpect(status().isOk())
-                .andReturn();
-
-        String responseContent = result.getResponse().getContentAsString();
-        return extractToken(responseContent);
-    }
-
-    private String extractToken(String responseContent) {
-        return responseContent.split(":")[1].replaceAll("[\"{}]", "").trim();
-    }*/
-
-
-    @Test
-    @WithMockUser(username = "testuser", password = "testpassword", roles = "ADMIN")
-    public void createTenWorks() throws Exception {
-        //String token = obtainAccessToken();
-        for (int i = 0; i <= 10; i++) {
-            String newWorkJson = String.format(
-                    "{\"name\":\"Entity %d\", \"description\": %d, \"cost\": %d}"
-                    , i, i * 10, i * 100);
-
-            mockMvc.perform(post("/work/save")
-                            //.header("Authorization", "Bearer " + token)
-                            .content(newWorkJson)
-                            .contentType("application/json"))
-                            .andExpect(status().isCreated());
-        }
-    }
-
+    
 }
